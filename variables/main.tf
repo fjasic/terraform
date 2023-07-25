bda9b76807cde4c37a99ef71166590cf3a14a969
fjasic <filipjasic8@gmail.com>
2023-07-25 15:08:00 +0200

    feat: challenge_1

--- variables/main.tf
+++ variables/main.tf
@@ -19,6 +19,36 @@ variable "list" {
 }
 
 variable "mymap" {
-  type = map()
+  type = map(string)
   default = {key1 = "value1"}
+}
+
+variable "inputname" {
+  type = string
+  description = "value"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = var.inputname
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+variable "mytuple" {
+  type = tuple([ string, number, string ])
+  default = ["cat", 1, "dog"]
+}
+
+variable "myobject" {
+  type = object({name = string, port = list(number)})
+  default = {
+    name = "FJ"
+    port = [22,25,80]
+  }
 }
